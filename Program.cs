7623314d1db78508316f83b31ab91bd65ca3230e
longpatrick <long.patrick1997@gmail.com>
2021-09-16 17:07:46 -0500

    Console.ReadLine()

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace IntroApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            Console.ReadLine();
         }
     }
 }
