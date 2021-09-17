bbcf5269cc9df34b75a98fe1562f41c1246b3467
longpatrick <long.patrick1997@gmail.com>
2021-09-17 17:56:26 -0500

    work with strings in C#

--- Program.cs
+++ Program.cs
@@ -10,17 +10,22 @@ namespace IntroApplication
     {
         static void Main(string[] args)
         {
-            string characterName = "John";
-            int characterAge = 35;
+            // string characterName = "John";
+            // int characterAge = 35;
 
-            Console.WriteLine("Hello World");
+            // Console.WriteLine("Hello World");
 
-            Console.WriteLine($"There once was a man named {characterName}.");
-            Console.WriteLine($"He was {characterAge} years old.");
-            Console.WriteLine($"He really liked the name {characterName}.");
-            Console.WriteLine($"But he did not like being {characterAge} years old.");
+            // Console.WriteLine($"There once was a man named {characterName}.");
+            // Console.WriteLine($"He was {characterAge} years old.");
+            // Console.WriteLine($"He really liked the name {characterName}.");
+            // Console.WriteLine($"But he did not like being {characterAge} years old.");
 
-            Console.ReadLine();
+            // Console.ReadLine();
+
+
+            // Working with data types in C#
+            string phrase = "C# is new to me";
+            
         }
     }
 }
