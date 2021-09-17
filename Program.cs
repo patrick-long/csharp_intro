270cc95f27ab5006e6900d2fefa8f7fd93c72a22
longpatrick <long.patrick1997@gmail.com>
2021-09-17 17:40:22 -0500

    use string interpolation to implement newly created variables

--- Program.cs
+++ Program.cs
@@ -15,10 +15,10 @@ namespace IntroApplication
 
             Console.WriteLine("Hello World");
 
-            Console.WriteLine("There once was a man named John.");
-            Console.WriteLine("He was 35 years old.");
-            Console.WriteLine("He really liked the name John.");
-            Console.WriteLine("But he did not like being 35 years old.");
+            Console.WriteLine($"There once was a man named {characterName}.");
+            Console.WriteLine($"He was {characterAge} years old.");
+            Console.WriteLine($"He really liked the name {characterName}.");
+            Console.WriteLine($"But he did not like being {characterAge} years old.");
 
             Console.ReadLine();
         }
